66f44e6ac7d97450b0b7f8f1ce8a7b3c6d0d7f3e
SQuazar <flawesv@yandex.ru>
2023-03-18 20:06:39 +0300

    Initial commit

---
+++ backend/src/test/java/net/quazar/backend/SinglePersonalAccountApplicationTest.java
@@ -0,0 +1,12 @@
+package net.quazar.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SinglePersonalAccountApplicationTest {
+
+    @Test
+    public void contextLoads() {}
+
+}
