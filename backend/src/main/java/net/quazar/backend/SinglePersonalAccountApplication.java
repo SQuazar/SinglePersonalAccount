9636cc95cfee32901083e906c2e8049e6e6f2f44
SQuazar <flawesv@yandex.ru>
2023-04-09 17:16:48 +0300

    Added initialization for FileUploadService

--- backend/src/main/java/net/quazar/backend/SinglePersonalAccountApplication.java
+++ backend/src/main/java/net/quazar/backend/SinglePersonalAccountApplication.java
@@ -1,13 +1,22 @@
 package net.quazar.backend;
 
+import net.quazar.backend.service.FileUploadService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class SinglePersonalAccountApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SinglePersonalAccountApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner init(FileUploadService service) {
+        return args -> service.init();
+    }
 }
